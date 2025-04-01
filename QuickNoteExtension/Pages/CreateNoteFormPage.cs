40adf92f23d1c9e2ef778161e63d058ea8197560
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2025-04-01 21:44:17 +0530

    Setup the Adaptive Card Form

--- QuickNoteExtension/Pages/CreateNoteFormPage.cs
+++ QuickNoteExtension/Pages/CreateNoteFormPage.cs
@@ -8,7 +8,7 @@ using System.Threading.Tasks;
 
 namespace QuickNoteExtension.Pages
 {
-    internal sealed partial class CreateNoteFormPage: ContentPage
+    internal sealed partial class CreateNoteFormPage : ContentPage
     {
         public CreateNoteFormPage() {
             Icon = IconHelpers.FromRelativePath("Assets\\StoreLogo.png");
@@ -18,9 +18,48 @@ namespace QuickNoteExtension.Pages
 
         public override IContent[] GetContent()
         {
-            return [
-                new MarkdownContent("# Hello, world!\n This is a **markdown** page."),
-            ];
+            return [formContent];
         }
+
+        readonly FormContent formContent = new()
+        {
+            TemplateJson = @"
+            {
+                ""type"": ""AdaptiveCard"",
+                ""$schema"": ""http://adaptivecards.io/schemas/adaptive-card.json"",
+                ""version"": ""1.6"",
+                ""body"": [
+                    {
+                        ""type"": ""TextBlock"",
+                        ""text"": ""Title"",
+                        ""wrap"": true
+                    },
+                    {
+                        ""type"": ""Input.Text"",
+                        ""id"": ""title"",
+                        ""placeholder"": ""Note title""
+                    },
+                    {
+                        ""type"": ""TextBlock"",
+                        ""text"": ""Contents"", 
+                        ""wrap"": true
+                    },
+                    {
+                        ""type"": ""Input.Text"",
+                        ""id"": ""content"",
+                        ""placeholder"": ""Write your note contents..."",
+                        ""isMultiline"": true
+                    }
+                ],
+                ""actions"": [
+                    {
+                        ""type"": ""Action.Submit"",
+                        ""title"": ""Save Note""
+                    }
+                ]
+            }
+        "
+        };
+
     }
 }
