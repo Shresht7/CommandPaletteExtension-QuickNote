ba2620540f8e8720b78a8574db5d85f189faa3e4
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2025-04-25 21:07:49 +0530

    Make file-extension configurable in the settings

--- QuickNoteExtension/Utils.cs
+++ QuickNoteExtension/Utils.cs
@@ -14,12 +14,22 @@ internal class Utils
         return QuickNoteExtensionSettings.Instance.NotesPath.Value ?? Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
     }
 
-    public static (string filePath, string fileName) NotePath(string Title = "note", string Extension = "txt")
+    public static (string filePath, string fileName) NotePath(string Title = "note")
     {
         string directory = NotesDirectory();
-        string fileName = $"{Title}-{Timestamp()}.{Extension}";
+        string fileName = $"{Title}-{Timestamp()}.{Extension()}";
         string filePath = Path.Combine(directory, fileName);
         return (filePath, fileName);
+    }
+
+    private static string Extension()
+    {
+        var extension = QuickNoteExtensionSettings.Instance.Extension.Value ?? "txt";
+        if (extension.StartsWith('.'))
+        {
+            extension = extension.Substring(1);
+        }
+        return extension;
     }
 
     private static string Timestamp()
