774e7e3f763e2b0f5500261b17e75ae90c21e793
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2025-04-26 15:44:13 +0530

    Add ViewNotePage for displaying and filtering notes
    Introduces a new `ViewNotePage` class derived from `DynamicListPage`, which allows users to view a list of notes. The class includes functionality to filter notes based on search input.
    
    Also updates `QuickNoteExtensionPage` to include a navigation option to the new "View Notes" feature.

---
+++ QuickNoteExtension/Pages/ViewNotePage.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.CommandPalette.Extensions;
+using Microsoft.CommandPalette.Extensions.Toolkit;
+
+namespace QuickNoteExtension.Pages
+{
+    internal sealed partial class ViewNotePage : DynamicListPage
+    {
+        public ViewNotePage()
+        {
+            Title = "View Note";
+            Icon = new("\uE70B");
+            Name = "View";
+            shownItems = items;
+        }
+
+        private ListItem[] items = new ListItem[]
+        {
+                   new ListItem(new NoOpCommand()) { Title = "One" },
+                   new ListItem(new NoOpCommand()) { Title = "Two" },
+                   new ListItem(new NoOpCommand()) { Title = "Three" },
+        };
+
+        private ListItem[] shownItems;
+
+        public override IListItem[] GetItems()
+        {
+            return shownItems;
+        }
+
+        public override void UpdateSearchText(string oldSearch, string newSearch)
+        {
+            if (oldSearch == newSearch)
+            {
+                shownItems = items;
+            }
+            else
+            {
+                shownItems = items.Where(i => i.Title.Contains(newSearch)).ToArray();
+            }
+
+            RaiseItemsChanged(shownItems.Length);
+        }
+    }
+}
